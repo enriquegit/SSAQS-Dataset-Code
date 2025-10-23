036b1b6b94b180c336b2a0a8ab90700f1b644898
Enrique <enrique.garcia.ceja@gmail.com>
2025-10-23 12:15:58 -0600

    First commit

---
+++ EDA-fitbit.R
@@ -0,0 +1,105 @@
+library(ggplot2)
+library(gridExtra)
+library(cowplot)
+
+# Path to root directory where the dataset resides.
+ROOT_DIR = "C://bigdata/SSAQS-Dataset/"
+
+# List of users ids.
+users <- list.dirs(ROOT_DIR, recursive = F, full.names = F)
+
+
+df.activity <- NULL
+df.hr <- NULL
+df.oxygen <- NULL
+df.sleep <- NULL
+df.steps <- NULL
+df.stress <- NULL
+
+# Concatenate all files.
+for(u in users){
+  
+  f <- paste0(ROOT_DIR,u,"/activity_level.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.activity <- rbind(df.activity, df)
+  }
+  
+  f <- paste0(ROOT_DIR,u,"/hr.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.hr <- rbind(df.hr, df)
+  }
+  
+  f <- paste0(ROOT_DIR,u,"/oxygen.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.oxygen <- rbind(df.oxygen, df)
+  }
+  
+  f <- paste0(ROOT_DIR,u,"/sleep.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.sleep <- rbind(df.sleep, df)
+  }
+  
+  f <- paste0(ROOT_DIR,u,"/steps.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.steps <- rbind(df.steps, df)
+  }
+  
+  f <- paste0(ROOT_DIR,u,"/stress.csv")
+  if(file.exists(f)){
+    df <- read.csv(f)
+    df.stress <- rbind(df.stress, df)
+  }
+  
+}
+
+# Plot histograms
+
+p1 <- ggplot(df.oxygen, aes(x = value)) +
+  geom_histogram() +
+  ggtitle("Oxygen saturation")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+p2 <- ggplot(df.hr, aes(x = rmssd)) +
+  geom_histogram() +
+  ggtitle("Heart rate (rmssd)")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+p3 <- ggplot(df.sleep, aes(x = deep_sleep_in_minutes)) +
+  geom_histogram() +
+  ggtitle("Deep sleep time (minutes)")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+p4 <- ggplot(df.steps, aes(x = steps)) +
+  geom_histogram() +
+  ggtitle("Steps")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+p5 <- ggplot(df.stress, aes(x = STRESS_SCORE)) +
+  geom_histogram() +
+  ggtitle("Stress score (Fitbit)")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+df <- as.data.frame(table(df.activity$level))
+names(df) <- c("level","count")
+
+p6 <- ggplot(df, aes(x = level, y = count)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  ggtitle("Activty level")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+#grid.arrange(p1, p2, p3, p4, p5, p6, ncol = 3, nrow = 3)
+
+plot_grid(p1, p2, p3, p4, p5, p6,
+          ncol = 3, nrow = 3,
+          labels = c("a)", "b)", "c)", "d)", "e)", "f)"))
---
+++ EDA-questions.R
@@ -0,0 +1,67 @@
+library(dygraphs)
+
+# Path to root directory where the dataset resides.
+ROOT_DIR = "C://bigdata/SSAQS-Dataset/"
+
+# List of users ids.
+users <- list.dirs(ROOT_DIR, recursive = F, full.names = F)
+
+
+# Compute the percent of answered days.
+percents <- NULL
+counts <- NULL
+
+for(u in users){
+  
+  df <- read.csv(paste0(ROOT_DIR,u,"/daily_questions.csv"))
+  
+  first.timestamp <- min(df$timeStampStop)
+  first.datetime <- as.POSIXct(first.timestamp, origin="1970-01-01", tz="UTC")
+  
+  last.timestamp <- max(df$timeStampStop)
+  last.datetime <- as.POSIXct(last.timestamp, origin="1970-01-01", tz="UTC")
+  
+  days_difference <- floor(as.numeric(difftime(last.datetime, first.datetime, units = "days"))) + 1
+  pct <- (nrow(df) / days_difference) * 100
+  
+  percents <- c(percents, pct)
+  counts <- c(counts, nrow(df))
+}
+
+mean(percents)
+
+summary(counts)
+
+
+# Validate data ranges.
+
+all <- NULL
+
+for(u in users){
+  df <- read.csv(paste0(ROOT_DIR,u,"/daily_questions.csv"))
+  all <- rbind(all, df)
+}
+
+hist(all$stress)
+
+summary(all)
+
+# Plot stress over time.
+
+u <- "1" # Select user.
+
+df <- read.csv(paste0(ROOT_DIR,u,"/daily_questions.csv"))
+
+df$timeStampStop <- as.POSIXct(df$timeStampStop,
+                                    origin="1970-01-01", tz="UTC")
+
+time_series <- df[, c("timeStampStop","stress")]
+
+dygraph(time_series) %>% 
+  dyAxis("y", label = "stress levels") %>%
+  dyOptions(fillGraph = F, drawGrid = F) %>%
+  dySeries("stress", label = "Stress Level") %>%
+  dyEvent(as.Date("2025-04-12"), "Start Easter", labelLoc = "bottom", color = "red", strokePattern = "dashed") %>%
+  dyEvent(as.Date("2025-04-20"), "End Easter", labelLoc = "bottom", color = "red", strokePattern = "dashed")
+
+
